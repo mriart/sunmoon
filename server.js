28a352ec3cd4422b8b8ffe8df6a79ff1b092b0e8
mriart <marc_riart@es.ibm.com>
2020-04-30 16:57:09 +0200

    v20200430 - Added delta

--- server.js
+++ server.js
@@ -7,17 +7,23 @@ var lat = 41.591158899999996;
 var lon = 1.5208624;
 
 const server = http.createServer((req, res) => {
-  var times = sc.getTimes(new Date(), lat, lon);
+  var date = new Date();
+  var dateYest = new Date(new Date().setDate(new Date().getDate()-1));
+
+  var times = sc.getTimes(date, lat, lon);
+  var timesYest = sc.getTimes(dateYest, lat, lon);
   var sunRise = times.sunrise.getHours() + ':' + times.sunrise.getMinutes();
   var sunDown = times.sunset.getHours() + ':' + times.sunrise.getMinutes();
-
-  var moon = sc.getMoonIllumination(new Date());
+  
+  var moon = sc.getMoonIllumination(date);
   var moonFrac = moon.fraction;
   moonFrac = Math.round(moonFrac * 100)/100;
   var moonPhase = moon.phase;
   moonPhase = Math.round(moonPhase * 100)/100;
 
-  resStr = "Sun Rise = " + sunRise +" UTC" + "\nSun Down = " + sunDown +" UTC";
+  delta = (times.sunrise.getTime() - (timesYest.sunrise.getTime() + 24*3600*1000))/1000;
+  resStr = "Delta since yesterday in s = " + delta;
+  resStr = resStr + "\nSun Rise = " + sunRise +" UTC (+2)" + "\nSun Down = " + sunDown +" UTC (+2)";
   resStr = resStr + "\n\nMoon Ilumination [0-1] = " + moonFrac + "\nMoon Phase [0:new, 0.5:full, 0.99:new] = " + moonPhase;
 
   res.statusCode = 200;
